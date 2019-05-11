bb4d0fb5aba6c2a9c9e88f233e48f078c3c57180
Aleksei Ryzhii <aleksei.ryzhyi@gmail.com>
2019-05-11 15:07:20 +0300

    recycler, adapter

---
+++ app/src/main/java/com/example/aleksei/repoinfo/RecyclerViewAdapter.java
@@ -0,0 +1,62 @@
+package com.example.aleksei.repoinfo;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecycleViewHolder> {
+
+    ArrayList<HashMap> mapArrayList;
+    Context context;
+
+    public RecyclerViewAdapter(Context context, ArrayList<HashMap> mapArrayList) {
+        this.mapArrayList = mapArrayList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecycleViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.list_item_repositories, viewGroup, false);
+        return new RecycleViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecycleViewHolder recycleViewHolder, int i) {
+
+        recycleViewHolder.list_item_tv_repositoryname.setText(mapArrayList.get(i).get("nameKey").toString());
+        recycleViewHolder.list_item_tv_starsnumber.setText(mapArrayList.get(i).get("starsKey").toString());
+        recycleViewHolder.list_item_tv_forksnumber.setText(mapArrayList.get(i).get("forksKey").toString());
+        recycleViewHolder.list_item_tv_watchesnumber.setText(mapArrayList.get(i).get("watchesKey").toString());
+    }
+
+    public class RecycleViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView list_item_tv_repositoryname;
+        private TextView list_item_tv_starsnumber;
+        private TextView list_item_tv_forksnumber;
+        private TextView list_item_tv_watchesnumber;
+
+        public RecycleViewHolder(@NonNull View itemView) {
+            super(itemView);
+            list_item_tv_repositoryname = itemView.findViewById(R.id.list_item_tv_repositoryname_text);
+            list_item_tv_starsnumber = itemView.findViewById(R.id.list_item_tv_starsnumber_text);
+            list_item_tv_forksnumber = itemView.findViewById(R.id.list_item_tv_forksnumber_text);
+            list_item_tv_watchesnumber = itemView.findViewById(R.id.list_item_tv_watchesnumber_text);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mapArrayList.size();
+    }
+}
---
+++ app/src/main/java/com/example/aleksei/repoinfo/RepositoriesActivity.java
@@ -0,0 +1,41 @@
+package com.example.aleksei.repoinfo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RepositoriesActivity extends Activity {
+
+    RecyclerView rvRepositories;
+    ArrayList<HashMap> mapArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repositories);
+
+        //test
+        mapArrayList = new ArrayList<>();
+        HashMap<String, String> hashMap;
+
+        for (int i = 0; i < 20; i++) {
+            hashMap = new HashMap<>();
+            hashMap.put("nameKey", "name");
+            hashMap.put("starsKey", "stars");
+            hashMap.put("forksKey", "forks");
+            hashMap.put("watchesKey", "watches");
+            mapArrayList.add(hashMap);
+        }
+        //testend
+
+        rvRepositories = findViewById(R.id.rv_repositories);
+        rvRepositories.setLayoutManager(new LinearLayoutManager(this));
+        rvRepositories.setAdapter(new RecyclerViewAdapter(this, mapArrayList));
+
+    }
+}
